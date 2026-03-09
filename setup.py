f5c1be7967d9ad55a49ddf60be78d30b75d317a0
hagikehappy <hagikehappy@163.com>
2026-03-09 12:10:05 +0800

    debug version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
-        'matplotlib>=2.1.0'
+        'matplotlib>=2.1.0',
         'numpy>=2.0.0',
     ],
     version='2.0.1',
